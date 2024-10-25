9d642562b4ddfd12ef8faac93bb9744667f8a966
omotomiwa26 <omotomiwa26@gmail.com>
2024-10-25 04:45:32 +0100

    REFACTOR: modify codes for app files

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('<h1 style="color: Blue";>Welcome To My-API-Project!</h1>');
+});
+
+module.exports = app;
